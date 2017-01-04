fa0a9e3b01ae3a9459d7676001b31dc828fc7edf
=argon <yunguta@gmail.com>
2017-01-04 23:44:23 +0000

    trying to fix static files error

--- lcidarkuman/production.py
+++ lcidarkuman/production.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c436yvsc216j$qb@(5#_4m%om#29+r=04zr$=n7z57e=q)p@m1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
