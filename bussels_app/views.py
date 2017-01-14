3e4dc14d229d5177a83e4f1616f1b66935ae1c4f
=argon <yunguta@gmail.com>
2017-01-14 15:59:00 +0000

    finish password change

--- bussels_app/views.py
+++ bussels_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.contrib.auth import hashers
+from django.contrib.auth.forms import PasswordChangeForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
@@ -184,6 +185,22 @@ def change_bussel_password(request):
 
         else:
             return HttpResponse(status=400)
+
+def change_user_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('accounts:change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'registration/change_password.html', { 
+        'form': form
+    })
 
 def update_bussel_location(request):
     code = request.GET["code"]
