1f5de31058ad3c5c24d851e8a127efe41e82ceaa
=argon <yunguta@gmail.com>
2017-03-26 07:45:48 +0000

    corrected settings.py

--- lcidarkuman/production.py
+++ lcidarkuman/production.py
@@ -115,6 +115,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-'''
+
 import dj_database_url
-DATABASES["default"] = dj_database_url.config()'''
+DATABASES["default"] = dj_database_url.config()
