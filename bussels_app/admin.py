bbdf4c90db32981bbf6340e5cff7e582ecba0a08
argon2008-aiti <macpherson2008@yahoo.com>
2018-07-10 18:38:40 +0000

    Update admin.py
    Added Bussell Member to admin interface

--- bussels_app/admin.py
+++ bussels_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from bussels_app.models import Bussel, BusselReport
+from bussels_app.models import Bussel, BusselReport, BussellMember
 
 class BusselAdmin(admin.ModelAdmin):
     exclude = ('bussel_location', 'code')
@@ -7,5 +7,9 @@ class BusselAdmin(admin.ModelAdmin):
 class BusselReportAdmin(admin.ModelAdmin):
     pass
 
+class BussellMemberAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Bussel, BusselAdmin)
 admin.site.register(BusselReport, BusselReportAdmin)
+admin.site.register(BussellMember, BussellMemberAdmin)
